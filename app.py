6cfdab9c70b224b1586d0926dce4f3c2aed6ca21
Dmytro Bosyi <d.bosyi@dvsts.com>
2023-05-11 07:35:59 +0300

    upd v

--- app.py
+++ app.py
@@ -1,33 +1,27 @@
 from flask import Flask, render_template, request
 from datetime import datetime
-import matplotlib.pyplot as plt
-import io
-import base64
-
+import pygal
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 
-    # Пример данных для графика
+	# Example data for the plot
     x = [1, 2, 3, 4, 5]
     y = [2, 4, 6, 8, 10]
 
-    # Построение графика
-    plt.plot(x, y)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Пример графика')
+    # Creating the line chart
+    line_chart = pygal.Line()
+    line_chart.title = 'Example Chart'
+    line_chart.x_labels = x
+    line_chart.add('Series', y)
 
-    # Преобразование графика в изображение в памяти
-    buffer = io.BytesIO()
-    plt.savefig(buffer, format='png')
-    buffer.seek(0)
-    image_base64 = base64.b64encode(buffer.read()).decode('utf-8')
+    # Rendering the chart to SVG format
+    chart_svg = line_chart.render()
 
-    # Возвращение HTML-страницы с встроенным графиком
-    return render_template('index.html', now=datetime.now(), plot_image=image_base64)	
+    # Returning the HTML page with the embedded chart SVG
+    return render_template('index.html', now=datetime.now(), plot_image=chart_svg)	
 
 if __name__ == "__main__":
 	app.run()
